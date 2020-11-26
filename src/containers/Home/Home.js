1d0af5c5184c8c19f0581001a16f9f28f6e8c2d5
ricardo aguiar <barrios.dev@gmail.com>
2020-11-26 06:19:35 +0100

    Update Home.js

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import '../../App.css';
 import { Link } from 'react-router-dom';
-// import GameLogoComponent from '../../components/GameLogoComponent/GameLogoComponent';
 import SpoonImageComponent from '../../components/SpoonImageComponent/SpoonImageComponent';
 import ForkImageComponent from '../../components/ForkImageComponent/ForkImageComponent';
 import Title from '../../components/Title/Title';
 import SubmitButton from '../../components/SubmitButton/SubmitButton';
-// import Footer from '../../components/FooterComponent/FooterComponent';
 import Layout from '../../components/Layout';
 
 export const Home = () => (
