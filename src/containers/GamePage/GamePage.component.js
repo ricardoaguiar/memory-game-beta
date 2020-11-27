50f28d8973af9eefd47ca7aad3404fdf3ec0f1e3
ricardoaguiar <barrios.dev@gmail.com>
2020-11-27 21:52:50 +0100

    added recipe page

--- src/containers/GamePage/GamePage.component.js
+++ src/containers/GamePage/GamePage.component.js
@@ -1,38 +1,3 @@
-// import React from "react"
-// import { Link } from "react-router-dom"
-// import Game from "../../components/Game/Game"
-// import SubmitButton from "../../components/SubmitButton/SubmitButton"
-// import ForkImageComponent from "../../components/ForkImageComponent/ForkImageComponent"
-// import Layout from "../../components/Layout"
-
-// export default class GamePage extends React.Component {
-//   getLevelTime = () => {
-//     const currentLevel = this.props.location.query.level
-//     if (currentLevel === "easy") return 6
-//     else if (currentLevel === "hard") return 8
-//   }
-//   getCardNumbers = () => {
-//     const currentLevel = this.props.location.query.level
-//     if (currentLevel === "easy") return 6
-//     else if (currentLevel === "hard") return 8
-//   }
-//   render() {
-//     const timeLevel = this.getLevelTime()
-//     const cardNumbers = this.getCardNumbers()
-//     return (
-//       <Layout>
-//         <ForkImageComponent />
-//         <Game timeLevel={timeLevel} cardNumbers={cardNumbers} />
-//         <div className="quitGame">
-//           <Link to="/pick-level-page">
-//             <SubmitButton buttonTitle="Quit Game" />
-//           </Link>
-//         </div>
-//       </Layout>
-//     )
-//   }
-// }
-
 import React from "react"
 import { Link } from "react-router-dom"
 import Game from "../../components/Game/Game"
