1f0086fa3866f03267db675378f12ece5961c67e
devrba <barrios.dev@gmail.com>
2021-03-02 08:07:09 +0100

    changed to new logo

--- src/components/GameLogoComponent/GameLogoComponent.js
+++ src/components/GameLogoComponent/GameLogoComponent.js
@@ -1,15 +1,15 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import './GameLogoComponent.styles.css';
+import React from "react"
+import { Link } from "react-router-dom"
+import "./GameLogoComponent.styles.css"
 
-import GameLogoImage from '../../assets/images/logo/logo_3x.png';
+import GameLogoImage from "../../assets/images/logo/GrandmaSita-Logo.png"
 
 export default function GameLogoComponent() {
   return (
     <header className="game-logo-image">
       <Link to="/">
         <img className="home " src={GameLogoImage} alt="Game Logo" />
-    </Link>
+      </Link>
     </header>
-  );
+  )
 }
